f80d61d43666cc0e01e2e4ecaf85d49f437f69db
staro2ia <pasha.st@gmail.com>
2018-10-23 20:14:56 +1000

    Initial commit

---
+++ app/src/main/java/edu/pavel/courceproject/MainActivity.kt
@@ -0,0 +1,21 @@
+package edu.pavel.courceproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val but = findViewById<Button>(R.id.button)
+
+        but.setOnClickListener { v ->
+            val tView = findViewById<TextView>(R.id.textView)
+            tView.text = "New text!"
+        }
+    }
+}
