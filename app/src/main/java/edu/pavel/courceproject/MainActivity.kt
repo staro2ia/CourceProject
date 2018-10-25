0c92abf8d57928e7a57dd33cdc5f9a2bfd57d259
staro2ia <pasha.st@gmail.com>
2018-10-25 13:10:00 +1000

    Создание таблицы

--- app/src/main/java/edu/pavel/courceproject/MainActivity.kt
+++ app/src/main/java/edu/pavel/courceproject/MainActivity.kt
@@ -2,20 +2,36 @@ package edu.pavel.courceproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var countRow = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val but = findViewById<Button>(R.id.button)
+        val table = findViewById<TableLayout>(R.id.tableMain)
 
-        but.setOnClickListener { v ->
-            val tView = findViewById<TextView>(R.id.textView)
-            tView.text = "New text!"
+        countRow += 5
+
+        for( i in 0 until countRow ) {
+            val row = TableRow(this)
+            row.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT)
+
+            val text = TextView(this)
+            text.text = i.toString()
+
+            row.addView(text, 0)
+
+            table.addView(row, i)
         }
+
     }
 }
