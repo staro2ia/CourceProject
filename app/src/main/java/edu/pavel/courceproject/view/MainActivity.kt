3e7b6b66ad68dd002976b54a58d980374396b8b1
staro2ia <pasha.st@gmail.com>
2026-03-11 09:48:30 +0300

    Update MainActivity.kt
    Почищен код.
    Логика загрузки перенесена в адаптер.
    Код использования сопрограмм переписан на современный стиль.

--- app/src/main/java/edu/pavel/courceproject/view/MainActivity.kt
+++ app/src/main/java/edu/pavel/courceproject/view/MainActivity.kt
@@ -3,13 +3,13 @@ package edu.pavel.courceproject.view
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import edu.pavel.courceproject.R
-import edu.pavel.courceproject.model.Film
 import edu.pavel.courceproject.model.FilmsTable
 import edu.pavel.courceproject.model.MyFilmsAdapter
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.net.URL
+import kotlinx.coroutines.withContext
 
 
 /**
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var listData: ListView
     private lateinit var adapter: MyFilmsAdapter
-    private lateinit var filmsTable: FilmsTable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,47 +26,28 @@ class MainActivity : AppCompatActivity() {
 
         listData = this.findViewById(R.id.listData)
 
-        GlobalScope.launch {
-            filmsTable = FilmsTable(this@MainActivity)
+        lifecycleScope.launch {
+            val filmsTable = FilmsTable(this@MainActivity)
 
-            loadData(filmsTable)
-
-            val films = filmsTable.selectAll()
+            val films = withContext(Dispatchers.IO) {
+                filmsTable.loadData()
+                filmsTable.selectAll()
+            }
 
             adapter = MyFilmsAdapter(applicationContext, films)
-
-            runOnUiThread {
-                listData.adapter = adapter
-            }
+            listData.adapter = adapter
 
             listData.setOnItemClickListener { _, _, position, _ ->
-                val film = adapter.getItem(position)
-                val detailIntent = FilmActivity.newIntent(this@MainActivity, film!!) // TODO: Исправить на нормально
-                startActivity(detailIntent)
+                adapter.getItem(position)?.let { film ->
+                    val detailIntent = FilmActivity.newIntent(this@MainActivity, film)
+                    startActivity(detailIntent)
+                }
             }
         }
-
     }
 
 //TODO: Add load data for other request.
 //TODO: Add feature for save data in local DB.
-
-    /**
-     * @brief Load data from server and populate them to DB.
-     *
-     * @param filmsTable – table of films from BD
-     *
-     */
-    private fun loadData(filmsTable: FilmsTable) {
-        val apiResponse: String = URL("https://ghibliapi.herokuapp.com/films").readText()
-        val gson = Gson()
-        val films: List<Film> = gson.fromJson(apiResponse, object : TypeToken<List<Film>>() {}.type)
-
-        for (film in films) {
-            filmsTable.insert(film)
-        }
-
-    }
 
 }
 
